1818e3e46f5263d6f7961069e3770c6de901a106
Axel Schwarz <axelschwarz@gmx.de>
2021-04-16 17:09:05 +0200

    Init

---
+++ AppSigner/AppSignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSignerApp.swift
+//  AppSigner
+//
+//  Created by Axel Schwarz on 16.04.21.
+//
+
+import SwiftUI
+
+@main
+struct AppSignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSigner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSigner
+//
+//  Created by Axel Schwarz on 16.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Magenta!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
